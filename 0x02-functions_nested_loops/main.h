451dd2253323481991294787c91dca2e95f38cb1
Wanemodinaka <fridaynwaobasi74@gmail.com>
2023-09-13 01:43:11 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,8 @@
 #ifndef MAIN_HEADER_FILE
 #define MAIN_HEADER_FILE
 
-int _putchar.c
-void print_alphabet(void)
+int _putchar(char c);
+void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
