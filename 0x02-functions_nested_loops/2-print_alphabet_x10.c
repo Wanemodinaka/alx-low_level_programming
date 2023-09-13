a2ef3a822fe79c01f27c85de38db96cb617ca11a
Wanemodinaka <fridaynwaobasi74@gmail.com>
2023-09-13 03:04:23 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -13,10 +13,10 @@ void print_alphabet_x10(void)
 	while (count < 10)
 	{
 		ch = 'a';
-		while (ch <= 'z';)
+		while (ch <= 'z')
 		{
 			_putchar(ch);
-			ch++
+			ch++;
 		}
 	_putchar('\n');
 	count++;
