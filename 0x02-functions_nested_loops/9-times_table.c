9f9af50fc965a4e297cd4d51f8860152ba5035bf
Wanemodinaka <fridaynwaobasi74@gmail.com>
2023-09-13 12:23:26 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,17 +11,17 @@ void times_table(void)
 
 	for (x = 0; x <= 9; x++)
 	{
-		for (y = 0; y <= 9; y++)
+		for (y =0; y <= 9; y++)
 		{
 			z = x * y;
-			if  (z > 9)
+			if  (z > )
 			{
 				u = z % 10;
 				d = (z - u) / 10;
 				_putchar(44);
 				_putchar(32);
 				_putchar(d + '0');
-				_putchar(u + '0');
+				_putchar (u + '0');
 			}
 			else
 			{
