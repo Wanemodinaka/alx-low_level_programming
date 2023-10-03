90f4ff20a6759c7fac6780d661091abdb1db9283
Wanemodinaka <fridaynwaobasi74@gmail.com>
2023-10-02 18:15:50 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -14,16 +14,16 @@ int main(int argc, char *argv[])
 {
 	int i, j, sum = 0;
 
-	if(argc == 1)
+	if (argc == 1)
 	{
 		printf("0\n");
 		return (0);
 	}
-	for(i = 1; i < argc; i++)
+	for (i = 1; i < argc; i++)
 	{
-		for(j = 0; argv[i][j] != '\0'; j++)
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			if(!isdigit(argv[i][j]))
+			if (!isdigit(argv[i][j]))
 			{
 				printf("Error\n");
 				return (1);
