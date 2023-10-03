7218963f593a103a4361d76aa5d9ec968a52adb6
Wanemodinaka <fridaynwaobasi74@gmail.com>
2023-10-02 17:21:53 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - multiples two number
+ * @argc: number of argument
+ * @argv: array of argument
+ * Return: 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int result, num1, num2;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	
+	result = num1 * num2;
+	printf("%d\n", result);
+	return (0);
+}
